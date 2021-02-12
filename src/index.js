e0f7609d7b622c3990e364297f06613c2225905f
Ekaterina Baliasnikova <balyasnikovak@gmail.com>
2021-02-12 11:44:59 +0200

    remove redux

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import {
 } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
